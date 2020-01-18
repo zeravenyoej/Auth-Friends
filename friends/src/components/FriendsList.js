6ed0dacf8cda3f47ad36ee9fb3d2d057351eb003
Joey Nevarez <josephnevarez12@gmail.com>
2020-01-18 13:52:22 -0800

    added boilerplate form in FriendsList

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FriendCard from './FriendsCard';
 
 const FriendsList = (props) => {
+    const [formValues, setFormValues] = useState({
+        name: '',
+        age: '',
+        email: '',
+        id: Date.now()
+    });
+
+    const handleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
         <div>
+            <form>
+                <input type='text' name='name' placeholder='Name' value={formValues.name} onChange={handleChange}/>
+                <input type='email' name='email' placeholder='Email' value={formValues.email} onChange={handleChange}/>
+                <input type='text' name='age' placeholder='Age' value={formValues.age} onChange={handleChange}/>
+                <button onClick={handleSubmit}>Add Friend</button>
+            </form>
+
             <FriendCard/>
         </div>
     );
